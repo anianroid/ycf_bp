5ba6cc70b50b806cfa1deb03c30aee834829ac22
Anirudh Madhavan <anianroid@gmail.com>
2018-02-27 01:07:18 -0500

    media queries

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
 import Home from "./Home";
-import NavBar from './NavBar';
-import Approach from './Approach';
-import Section from './DefaultSection';
+import NavBar from "./NavBar";
+import Approach from "./Approach";
+import Section from "./DefaultSection";
 require("./styles.css");
 
 const App = () => (
   <div>
     <div className="landImage">
-      <img src="http://via.placeholder.com/1440x748" />
+      <img src="http://via.placeholder.com/1440x648" />
     </div>
     <NavBar />
     <Home />
